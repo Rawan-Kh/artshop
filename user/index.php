c65a9d119392ef64c2bd5ae5e1e8436997336ba4
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2021-10-06 22:48:00 +0200

    Update and rename index.html to index.php

--- user/index.php
+++ user/index.php
@@ -1,70 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
-  <!-- Box icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
-
-  <!-- Custom StyleSheet -->
-  <link rel="stylesheet" href="./css/styles.css" />
-  <title>Codevo - Ecommerce Website</title>
-</head>
-
-<body>
-  <!-- Header -->
-  <header id="home" class="header">
-    <!-- Navigation -->
-    <nav class="nav">
-      <div class="navigation container">
-        <div class="logo">
-          <h1>Codevo</h1>
-        </div>
-
-        <div class="menu">
-          <div class="top-nav">
-            <div class="logo">
-              <h1>Codevo</h1>
-            </div>
-            <div class="close">
-              <i class="bx bx-x"></i>
-            </div>
-          </div>
-
-          <ul class="nav-list">
-            <li class="nav-item">
-              <a href="#home" class="nav-link scroll-link">Home</a>
-            </li>
-            <li class="nav-item">
-              <a href="product.html" class="nav-link">Products</a>
-            </li>
-            <li class="nav-item">
-              <a href="#about" class="nav-link scroll-link">About</a>
-            </li>
-            <li class="nav-item">
-              <a href="#contact" class="nav-link scroll-link">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a href="#account" class="nav-link scroll-link">Account</a>
-            </li>
-            <li class="nav-item">
-              <a href="cart.html" class="nav-link icon"><i class="bx bx-shopping-bag"></i></a>
-            </li>
-          </ul>
-        </div>
-
-        <a href="cart.html" class="cart-icon">
-          <i class="bx bx-shopping-bag"></i>
-        </a>
-
-        <div class="hamburger">
-          <i class="bx bx-menu"></i>
-        </div>
-      </div>
-    </nav>
+<?php
+include "header.php";
+$link=mysqli_connect("localhost","root","");
+mysqli_select_db($link,"artshop");
+?>
 
     <!-- Hero -->
     <img src="./images/banner.png" alt="" class="hero-img" />
@@ -75,7 +13,7 @@
         <span>lorem ipsum</span>
         <span>and lorem ipsum</span>
       </h1>
-      <a class="btn" href="#">Discover</a>
+      <a class="btn" href="#testimonial">Discover</a>
     </div>
   </header>
 
@@ -430,7 +368,7 @@
     <section class="section">
      
       <div class="testimonial-center container">
-        <div class="testimonial">
+        <div id="testimonial" class="testimonial">
           
           <h4>prints</h4>
           <p>
@@ -441,7 +379,7 @@
           <div class="img-cover">
             <!-- <img src="./images/profile1.jpg" alt="" /> -->
           </div>
-          <a href="#" class="cta2">read more &rightarrow;</a>
+          <a href="#" class="cta2">view &rightarrow;</a>
         </div>
         <div class="testimonial">
           
@@ -454,7 +392,7 @@
           <div class="img-cover">
             <!-- <img src="./images/profile2.jpg" alt="" /> -->
           </div>
-          <a href="#" class="cta2">read more &rightarrow;</a>
+          <a href="shop.php" class="cta2">view &rightarrow;</a>
         </div>
         <div class="testimonial">
           
@@ -467,7 +405,7 @@
           <div class="img-cover">
             <!-- <img src="./images/profile3.jpg" alt="" /> -->
           </div>
-          <a href="games.html" class="cta2">enjoy &rightarrow;</a>
+          <a href="games.php" class="cta2">enjoy &rightarrow;</a>
         </div>
       </div>
     </section> 
@@ -497,71 +435,6 @@
     </section> -->
   </main>
 
-  <!-- Footer -->
-  <footer id="footer" class="section footer">
-    <div class="container">
-      <div class="footer-container">
-        <div class="footer-center">
-          <h3>EXTRAS</h3>
-          <a href="#">Brands</a>
-          <a href="#">Gift Certificates</a>
-          <a href="#">Affiliate</a>
-          <a href="#">Specials</a>
-          <a href="#">Site Map</a>
-        </div>
-        <div class="footer-center">
-          <h3>INFORMATION</h3>
-          <a href="#">About Us</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="#">Contact Us</a>
-          <a href="#">Site Map</a>
-        </div>
-        <div class="footer-center">
-          <h3>MY ACCOUNT</h3>
-          <a href="#">My Account</a>
-          <a href="#">Order History</a>
-          <a href="#">Wish List</a>
-          <a href="#">Newsletter</a>
-          <a href="#">Returns</a>
-        </div>
-        <div class="footer-center">
-          <h3>CONTACT US</h3>
-          <div>
-            <span>
-              <i class="fas fa-map-marker-alt"></i>
-            </span>
-            42 Dream House, Dreammy street, 7131 Dreamville, USA
-          </div>
-          <div>
-            <span>
-              <i class="far fa-envelope"></i>
-            </span>
-            company@gmail.com
-          </div>
-          <div>
-            <span>
-              <i class="fas fa-phone"></i>
-            </span>
-            456-456-4512
-          </div>
-          <div>
-            <span>
-              <i class="far fa-paper-plane"></i>
-            </span>
-            Dream City, USA
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-  </footer>
-  <!-- End Footer -->
-
-  <!-- GSAP -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
-  <!-- Custom Script -->
-  <script src="./js/index.js"></script>
-</body>
-
-</html>
+  <?php
+  include "footer.php";
+  ?>
