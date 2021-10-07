5edc955cef9475ef53a5640a3ad92247675e2e19
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2021-10-07 21:57:46 +0200

    Add files via upload

---
+++ user/footer.php
@@ -0,0 +1,86 @@
+<footer id="footer"><!--Footer-->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                
+                   
+                
+         
+             
+             
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-widget">
+    <div class="col-sm-3 col-sm-offset-1">
+                    <div class="single-widget">
+                        <h2>About Shopper</h2>
+                        <form action="#" class="searchform">
+                            <input type="text" placeholder="Your email address" />
+                            <button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+                            <p>Get the most recent updates from <br />our site and be updated your self...</p>
+                        </form>
+                    </div>
+                </div>
+        <div class="container">
+            <div class="row">
+               
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>categories</h2>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="">T-Shirt</a></li>
+                            <li><a href="">Mens</a></li>
+                            <li><a href="">Womens</a></li>
+                            <li><a href="">Gift Cards</a></li>
+                            <li><a href="">Shoes</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>follow us</h2>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="">Terms of Use</a></li>
+                            <li><a href="">Privecy Policy</a></li>
+                            <li><a href="">Refund Policy</a></li>
+                            <li><a href="">Billing System</a></li>
+                            <li><a href="">Ticket System</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>contact us</h2>
+                      <p>Matt LeBlanc Art Inc. info@MattLeBlancArt.com</p>
+                      <p>Cairo ,Egypt</p>
+                    </div>
+                </div>               
+
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="footer-bottom"> -->
+        <div class="container">
+            <div class="row">
+                <p class="pull-left">Copyright © 2021 E-Shopper. All rights reserved.</p>
+                
+            </div>
+        </div>
+    <!-- </div> -->
+
+</footer><!--/Footer-->
+
+
+
+<!-- <script src="js/jquery.js"></script>
+<script src="js/price-range.js"></script>
+<script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/main.js"></script> -->
+</body>
+</html>
